bb57ca6549bffd04da43aa8c614a34c0753714f4
odino4ka49 <TessaMarizza@gmail.com>
2016-02-02 16:58:29 +0600

    Tools start

---
+++ configui/webvepp/static/webvepp/js/MainTools.js
@@ -0,0 +1,32 @@
+var WEBVEPP = WEBVEPP||{};
+WEBVEPP.namespace = function (ns_string) {
+    var parts = ns_string.split('.'),
+        parent = WEBVEPP,
+        i;
+    if (parts[0] === "WEBVEPP") {
+        parts = parts.slice(1);
+    }
+    for (i = 0; i < parts.length; i += 1) {
+        if (typeof parent[parts[i]] === "undefined") {
+            parent[parts[i]] = {};
+        }
+        parent = parent[parts[i]];
+    }
+    return parent;
+};
+WEBVEPP.serveradr = function(){
+	return location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '')+"/";
+};
+
+$(document).on("set_loading_cursor",function(){
+    document.body.style.cursor='wait';
+});
+$(document).on("unset_loading_cursor",function(){
+    document.body.style.cursor='default';
+});
+$(document).ready(function(){
+    var scheme = WEBVEPP.List(),
+        menu_bar = WEBVEPP.MenuBar();
+});
+
+
