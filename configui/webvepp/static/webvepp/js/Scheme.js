e7e31eaac6f29a9a2c4da2602a8e7b3661bbbdf1
odino4ka49 <TessaMarizza@gmail.com>
2016-04-08 17:25:00 +0600

    V4 Shemes

--- configui/webvepp/static/webvepp/js/Scheme.js
+++ configui/webvepp/static/webvepp/js/Scheme.js
@@ -1,9 +1,11 @@
 WEBVEPP.namespace("WEBVEPP.Scheme");
 WEBVEPP.Scheme = function(settings){
-    var scheme_names = {"system":"","sample":""},
+    var scheme_names = {},
         settings = settings;
     var scheme, schemeSVG,
         svg,
+        scheme_button=null,
+        switch_bar = d3.select("#Scheme_switcher_bar"),
 
         zoom = d3.behavior.zoom()
         .scale(4)
@@ -11,16 +13,90 @@ WEBVEPP.Scheme = function(settings){
             svg.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
         }),
 
-        getSchemeName = function(){
+        //заглушка
+        setSchemeSample = function(){
             var system_name = settings.getSystemName();
             switch(system_name){
                 case "CHAN":
-                    return "Chan_V3-V4_scheme_bbg"
+                    scheme_names = {
+                        "name": "Scheme",
+                        "type": "single",
+                        "schemes": [
+                          {
+                            "name": "Main",
+                            "filename": "Chan_V3-V4_scheme_bbg"
+                          }
+                        ]
+                      }
+                    break;
                 case "V4":
-                    return "vepp4_main"
+                    scheme_names = {
+                        "name": "Scheme",
+                        "type": "list",
+                        "schemes": [
+                            {
+                              "name":"Main",
+                              "filename": "vepp4_main"
+                            },
+                            {
+                              "name":"Non-linear",
+                              "filename": "vepp4_RF_elstat_non-linear",
+                            },
+                            {
+                              "name":"Orbit",
+                              "filename": "vepp4_corr_X_Z",
+                            },
+                            {
+                              "name":"Gradient",
+                              "filename": "vepp4_gradient"
+                            }
+                        ]
+                    }
+                    break;
                 default:
-                    return null
-                }
+                    scheme_names = {"type":null,"schemes":{}}
+                    break;
+               }
+        },
+
+        drawButtons = function(){
+            if(scheme_names.type==null || scheme_names.type=="single")
+                return;
+            var buttons = switch_bar.selectAll("li")
+                .data(scheme_names.schemes);
+            var button = buttons.enter();
+            button.append("li");
+
+            var button_update = buttons;
+
+            button_update
+                .on('click', function(d){
+                   scheme_button = d.name;
+                   reloadScheme(d.filename);
+                })
+                .text(function(d) {
+                    if(d.name)
+                        return d.name;
+                });
+            button_remove = buttons.exit()
+                .remove();
+        },
+
+        getSchemeName = function(){
+            if(scheme_names.type=="single"||scheme_button==null){
+                return $.grep(scheme_names.schemes, function(e){ return e.name == "Main"; })[0].filename;
+            }
+            else{
+                return $.grep(scheme_names.schemes, function(e){ return e.name == "scheme_button"; }).filename;
+            }
+        },
+
+        reloadScheme = function(scheme_name){
+            myNode = document.getElementById("schemeSVG");
+            while (myNode.firstChild) {
+                myNode.removeChild(myNode.firstChild);
+            }
+            loadScheme(scheme_name);
         },
 
         loadScheme = function(scheme_name){
@@ -29,6 +105,7 @@ WEBVEPP.Scheme = function(settings){
                 return;
             d3.xml(WEBVEPP.serveradr()+"static/webvepp/bg/"+scheme_name+".svg", "image/svg+xml", function(error, xml) {
                 if (error) throw error;
+                console.log(xml);
                 var defs = xml.documentElement.getElementById("defs");
                 document.getElementById("schemeSVG").appendChild(defs);
                 scheme = xml.documentElement.getElementById("Слой_x0020_1");
@@ -57,6 +134,8 @@ WEBVEPP.Scheme = function(settings){
         .call(zoom)
         .append('g')
         .attr('id','schemeSVG');
+    setSchemeSample();
+    drawButtons();
     loadScheme(getSchemeName());
 
     return {
